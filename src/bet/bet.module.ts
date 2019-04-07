75f015fbd1a505fbe51c8172a71b6c9d464d0b6e
riteshbxr <riteshbxr@gmail.com>
2019-04-07 08:04:06 +0530

    minor fix

--- src/bet/bet.module.ts
+++ src/bet/bet.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { BetController } from './bet.controller';
 import { BetService } from './bet.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Match } from 'dist/match/match.entity';
-import { Team } from 'dist/src/team/team.entity';
+import { Match } from 'src/match/match.entity';
+import { Team } from 'src/team/team.entity';
 import { Bet } from './bet.entity';
 
 @Module({
