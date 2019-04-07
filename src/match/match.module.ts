2d8210690a694fc24fba702c57dbfbdae1b3fff0
riteshbxr <riteshbxr@gmail.com>
2019-04-07 08:08:06 +0530

    more fixes

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MatchController } from 'dist/src/match/match.controller';
-import { MatchService } from 'dist/src/match/match.service';
+import { MatchController } from 'src/match/match.controller';
+import { MatchService } from 'src/match/match.service';
 import { Match } from './match.entity';
 
 @Module({
